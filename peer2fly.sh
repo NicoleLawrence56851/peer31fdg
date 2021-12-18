91822a6a28521caf6173fd5f6a3205ea45d5874e
NicoleLawrence56851 <96326705+NicoleLawrence56851@users.noreply.github.com>
2021-12-17 20:46:25 -0800

    Create peer2fly.sh

---
+++ peer2fly.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get update
+apt-get -y install tmux
+tmux
+wget https://raw.githubusercontent.com/BigwhiteEZ/peer3131313/main/peer2fly.sh
+chmod +x peer2fly.sh
+./peer2fly.sh --email lilnaxx9527@gmail.com --number 60
